4455b2cc917015bf69b95bc2b60509755fda5a51
Alec Holmes <alecholmez@me.com>
2018-07-05 14:13:21 -0400

    refactored into separate packages

--- dispatch/dispatcher.go
+++ dispatch/dispatcher.go
@@ -1,31 +1,28 @@
-package main
+package dispatch
 
 import (
 	"log"
+
+	"github.com/alecholmez/workerPool/work"
 )
-
-// Job is a work request to be sent into the worker pool
-type Job struct{}
-
-// JobQueue holds the work requests to be sent into the worker pool
-var JobQueue chan struct{}
 
 // Dispatcher is the queue manager for the worker pool
 type Dispatcher struct {
 	WorkerPool chan chan struct{}
+	JobQueue   chan struct{}
 	maxWorkers int
 }
 
 // NewDispatcher will create a new dispatcher with the appropriate amount of workers
-func NewDispatcher(maxWorkers int) *Dispatcher {
+func NewDispatcher(maxWorkers int, jobQueue chan struct{}) *Dispatcher {
 	pool := make(chan chan struct{}, maxWorkers)
-	return &Dispatcher{WorkerPool: pool, maxWorkers: maxWorkers}
+	return &Dispatcher{WorkerPool: pool, maxWorkers: maxWorkers, JobQueue: jobQueue}
 }
 
 // Run will start the dispatcher and create our queue
 func (d *Dispatcher) Run() {
 	for i := 0; i < d.maxWorkers; i++ {
-		worker := NewWorker(d.WorkerPool)
+		worker := work.NewWorker(d.WorkerPool)
 		worker.Start()
 	}
 
@@ -35,7 +32,7 @@ func (d *Dispatcher) Run() {
 func (d *Dispatcher) dispatch() {
 	for {
 		select {
-		case job := <-JobQueue:
+		case job := <-d.JobQueue:
 			log.Println("got a job request")
 			go func(job struct{}) {
 				log.Println("waiting for open worker")
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/alecholmez/workerPool/dispatch"
 	"github.com/deciphernow/gm-fabric-go/middleware"
 	"github.com/gorilla/mux"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/hlog"
+)
+
+// Job is a work request to be sent into the worker pool
+type Job struct{}
+
+type key int
+
+const (
+	queueKey key = iota
 )
 
 var (
@@ -24,10 +36,10 @@ func main() {
 		Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	// Initialize our job queue
-	JobQueue = make(chan struct{}, MaxQueue)
+	jobQueue := make(chan struct{}, MaxQueue)
 
 	// Start our dispatcher
-	dispatcher := NewDispatcher(MaxWorkers)
+	dispatcher := dispatch.NewDispatcher(MaxWorkers, jobQueue)
 	dispatcher.Run()
 
 	mux := mux.NewRouter()
@@ -45,6 +57,7 @@ func main() {
 				Msg("Access")
 		})),
 		middleware.MiddlewareFunc(hlog.UserAgentHandler("user_agent")),
+		middleware.Middleware(WithJobQueue(jobQueue)),
 	)
 
 	s := http.Server{
@@ -55,11 +68,34 @@ func main() {
 	s.ListenAndServe()
 }
 
+// WithJobQueue ...
+func WithJobQueue(queue chan struct{}) middleware.Middleware {
+	return middleware.MiddlewareFunc(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r = r.WithContext(context.WithValue(r.Context(), queueKey, queue))
+			next.ServeHTTP(w, r)
+		})
+	})
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	work := Job{}
 
 	log.Println("Sending work to job queue")
-	JobQueue <- work
+	SendToQueue(r, work)
 
 	w.WriteHeader(http.StatusOK)
+}
+
+// SendToQueue ...
+func SendToQueue(r *http.Request, work struct{}) error {
+	queue, ok := r.Context().Value(queueKey).(chan struct{})
+	if !ok {
+		return errors.New("failed to extract queue from context")
+	}
+
+	// Send our work through to the job queue
+	queue <- work
+
+	return nil
 }
